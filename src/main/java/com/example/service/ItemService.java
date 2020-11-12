69a05e0bd60cedde5491617241ea3eba4c74f4bd
原　奈津実 <natsumi@Natsumis-PC.local>
2020-11-12 13:28:10 +0900

    Initial Commit

---
+++ src/main/java/com/example/service/ItemService.java
@@ -0,0 +1,61 @@
+package com.example.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.example.domain.Item;
+import com.example.repository.ItemRepository;
+
+@Service
+@Transactional
+public class ItemService {
+	@Autowired
+	ItemRepository itemRepository;
+	
+	/**
+	 * 商品一覧取得サービス
+	 * @return List<item>
+	 */
+	public List<Item> findAll() {
+		return itemRepository.findAll();
+	}
+	
+	/**
+	 * 商品登録サービス
+	 * @param item
+	 * @return item
+	 */
+	public Item create(Item item) {
+		return itemRepository.save(item);
+	}
+	/**
+	 * 商品削除サービス
+	 * @param id
+	 */
+	public void delete(Integer id) {
+		itemRepository.deleteById(id);
+	}
+	
+	/**
+	 * 商品更新サービス
+	 * @param item
+	 * @return item
+	 */
+	public Item update(Item item) {
+		return itemRepository.save(item);
+	}
+
+	/** ID検索   */
+	public Optional<Item> findById(Integer id) {
+		// TODO 自動生成されたメソッド・スタブ
+		return itemRepository.findById(id);
+	}
+
+
+
+
+}
