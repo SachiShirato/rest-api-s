35ddefd8a016cbd5a6f3091edff4fba599c51757
webma <webma@192.168.1.8>
2020-12-15 13:51:33 +0900

    資源格納

--- src/test/java/com/example/mq/mqtestmain.java
+++ src/test/java/com/example/mq/mqtestmain.java
@@ -2,6 +2,7 @@ package com.example.mq;
 
 import java.io.IOException;
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -14,63 +15,76 @@ import com.ibm.msg.client.wmq.compat.base.internal.MQQueueManager;
 
 public class mqtestmain {
 
-	
 //編集箇所　1 共通		
-	private static String hostname = "localhost";
-	private static String channel = "SYSTEM.BKR.CONFIG";
-	private static int port = 50014;
-	private static String MQQueueManagerName = "QMFH01";
-	private static String accessQueueName = "QL.DH.ERR";
-	private static MQQueueManager qmgr;
-	private static MQQueue queue;		
-	
-	@BeforeEach
-	void setUp() throws Exception {	
-		System.out.println("共通設定");
-	
-		MQEnvironment.hostname = mqtestmain.hostname;
-		MQEnvironment.channel = mqtestmain.channel;
-		MQEnvironment.port = mqtestmain.port;
+	private static final String hostname = "localhost";
+	private static final String channel = "SYSTEM.BKR.CONFIG";
+	private static final int port = 50014;
+	private static final String MQQueueManagerName = "QMFH01";
+	private static final String accessQueueName = "QL.DH.ERR";
+//	private static MQQueueManager qmgr;
+//	private static MQQueue queue;
 
+	@BeforeAll
+	static void setUpAll() throws Exception {
+		System.out.println("クラス共通設定");
+
+		MQEnvironment.hostname = hostname;
+		MQEnvironment.channel = channel;
+		MQEnvironment.port = port;
 	}
 
+	@BeforeEach
+	void setUp() throws Exception {
+		System.out.println("共通設定");
+//
+//		MQEnvironment.hostname = mqtestmain.hostname;
+//		MQEnvironment.channel = mqtestmain.channel;
+//		MQEnvironment.port = mqtestmain.port;
+//
+	}
 
 	@Test
 	void test() {
+		MQQueueManager qmgr = null;
+		MQQueue queue = null;
 		try {
 //編集箇所　2			
-			int priority = 5;	
-			int characterSet = 943;	
-			String massage = "This is a message";	
+			int priority = 5;
+			int characterSet = 943;
+			String massage = "This is a message";
 
-			
 			qmgr = new MQQueueManager(mqtestmain.MQQueueManagerName);
+			
 			int openOption = MQC.MQOO_INPUT_AS_Q_DEF | MQC.MQOO_OUTPUT;
 			queue = qmgr.accessQueue(mqtestmain.accessQueueName, openOption);
-			
-			
+
 //put 
 			MQPutMessageOptions mqpmo = new MQPutMessageOptions();
-			mqpmo.options = MQC.MQPMO_NO_SYNCPOINT;	
-			queue.put(mqstb.mqput(priority,characterSet,massage));	
-				
-			
+			mqpmo.options = MQC.MQPMO_NO_SYNCPOINT;
+			queue.put(mqstb.mqput(priority, characterSet, massage));
+
 //get
-			System.out.println(mqstb.mqget(queue,17));
-			
-			
+			System.out.println(mqstb.mqget(queue, 17));
+
 //エラー投げている	throw new MQException(0, 9999, "dd");
-			
-			
+
 		} catch (MQException | IOException e) {
 			System.out.println("IOException occurred");
 			e.printStackTrace();
 		} finally {
 			try {
 				queue.close();
-				qmgr.disconnect();
-				System.out.println("close");
+				System.out.println("qClose");
 			} catch (MQException e) {
+//				queue =null;
+				e.printStackTrace();
+			}
+
+			try {
+				qmgr.disconnect();
+				System.out.println("qmgrClose");
+			} catch (MQException e) {
+//				qmgr =null;
 				e.printStackTrace();
 			}
 		}
